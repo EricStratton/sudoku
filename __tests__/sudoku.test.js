0c01dc0e2d23ac9733ad437ae304c2e5692c50b9
faustlarsen <faustlarsen@gmail.com>
2020-11-10 16:08:56 -0700

    pass third test
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -2,20 +2,24 @@ import Matrix from '../src/sudoku.js';
 
 describe('Matrix', () => {
 
-  test('it should create a blank row with 9 elements', () => {
-    const newMatrix = new Matrix();
-    expect(newMatrix.createRows()).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0])
-  });
+  // test('it should create a blank row with 9 elements', () => {
+  //   const newMatrix = new Matrix();
+  //   expect(newMatrix.createRows()).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0])
+  // });
 
-  test('it should create a complete 9x9 matrix', () => {
-    const newMatrix = new Matrix();
-    newMatrix.createRows();
-    expect(newMatrix.createMatrix()).toEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0]])
-  });
+  // test('it should create a complete 9x9 matrix', () => {
+  //   const newMatrix = new Matrix();
+  //   newMatrix.createRows();
+  //   expect(newMatrix.createMatrix()).toEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0]])
+  // });
+
+
+    test('it should sort and compare numbers within an array', () => {
+      const newMatrix = new Matrix();
+      expect(newMatrix.checkRow()).toEqual(true);
+
+    });
 });
-
-  
-
 
 
 
--- src/sudoku.js
+++ src/sudoku.js
@@ -5,51 +5,47 @@
 
 
 
-export default function Matrix() {
-  this.rows = [];
-  this.completeMatrix = [];
-}
-
-Matrix.prototype.createRows = function () {
-  for (let i = 0; i < 9; i++) {
-    this.rows.push(0);
-  }
-  return this.rows;
-}
-
-Matrix.prototype.createMatrix = function () {
-  for (let j = 0; j < 9; j++) {
-    this.completeMatrix.push(this.rows);
-  }
-  return this.completeMatrix;
-};
-
-// const newMatrix = new Matrix();
-// newMatrix.createRows();
-// newMatrix.createMatrix();
-
-
-// Matrix.prototype.makeRows = function () {
-
+// export default function Matrix() {
+//   this.rows = [];
+//   this.completeMatrix = [];
 // }
-// for (let i=0;i<9;i++){
-//  for(let j=0;j<9; j++){
- //   this.completeMatrix[i][j].push(rows);
- //i=0 completeMatrix[0]
- // }
-//}
-//   column.forEach(function(j){
-//     this.completeMatrix.push(0);
-//
-//   })
-// })
-// sudoku= math.matrix([[0, 0, 0, 0, 0, 0, 0, 0, 0], 
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0]]);
-//   if sudoku[0][0]===0;
+
+// Matrix.prototype.createRows = function () {
+//   for (let i = 0; i < 9; i++) {
+//     this.rows.push(0);
+//   }
+//   return this.rows;
+// }
+
+// Matrix.prototype.createMatrix = function () {
+//   for (let j = 0; j < 9; j++) {
+//     this.completeMatrix.push(this.rows);
+//   }
+//   return this.completeMatrix;
+// };
+
+export default function Matrix() {
+  this.completeMatrix = [[4, 3, 5, 2, 6, 9, 7, 8, 1], [6, 8, 2, 5, 7, 1, 4, 9, 3], [1, 9, 7, 8, 3, 4, 5, 6, 2], [8, 2, 6, 1, 9, 5, 3, 4, 7], [3, 7, 4, 6, 8, 2, 9, 1, 5], [9, 5, 1, 7, 4, 3, 6, 2, 8], [5, 1, 9, 3, 2, 6, 8, 7, 4], [2, 4, 8, 9, 5, 7, 1, 3, 6], [7, 6, 3, 4, 1, 8, 2, 5, 9]];
+}
+
+
+Matrix.prototype.checkRow = function () {
+  let array = this.completeMatrix[0];
+  array.sort();
+  for (let i = 0; i < 7; i++) {
+    if (array[i] < array[i + 1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+// method
+// sample=sort(this.completeMatrix[0][8])
+// method
+// Each row contains all numbers from 1 to 9 with no repeats.
+// for j
+// let sample=sort(this.completeMatrix[0])
+//         0,1,2,3,4,5,6,7,8 
+// sample=[1,2,3,4,5,6,7,8,9],[]
