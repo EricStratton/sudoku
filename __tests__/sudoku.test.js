3f581cf47895cc0351f3f2a5ecbe8fa39abd4342
faustlarsen <faustlarsen@gmail.com>
2020-11-10 17:41:57 -0700

    conundrum
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>
    Co-authored-by: Nataliya Zhuravleva <natalindria@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -14,11 +14,22 @@ describe('Matrix', () => {
   // });
 
 
-    test('it should sort and compare numbers within an array', () => {
+    test('it should sort and compare numbers within first array', () => {
       const newMatrix = new Matrix();
       expect(newMatrix.checkRow()).toEqual(true);
 
+    })
+
+    test('it will sort numbers in each array', () => {
+      const newMatrix = new Matrix();
+      newMatrix.sortCheckingArray();
+      expect(this.sortedMatrix[4]).toEqual([1,2,3,4,5,6,7,8,9]);
+
     });
+    // test
+    // const newMatrix = new Matrix();
+    // newMatrix.checkRow();
+    // expect(newMatrix[i].)
 });
 
 
--- src/sudoku.js
+++ src/sudoku.js
@@ -26,8 +26,21 @@
 
 export default function Matrix() {
   this.completeMatrix = [[4, 3, 5, 2, 6, 9, 7, 8, 1], [6, 8, 2, 5, 7, 1, 4, 9, 3], [1, 9, 7, 8, 3, 4, 5, 6, 2], [8, 2, 6, 1, 9, 5, 3, 4, 7], [3, 7, 4, 6, 8, 2, 9, 1, 5], [9, 5, 1, 7, 4, 3, 6, 2, 8], [5, 1, 9, 3, 2, 6, 8, 7, 4], [2, 4, 8, 9, 5, 7, 1, 3, 6], [7, 6, 3, 4, 1, 8, 2, 5, 9]];
+  //this.sortedMatrix = [];
 }
 
+const newMatrix=new Matrix();
+console.log(newMatrix.completeMatrix);
+Matrix.prototype.sortCheckingArray = function () {
+  console.log(this.completeMatrix);
+  let workArray = this.completeMatrix;
+  console.log(workArray);
+  for (let i = 0; i < 9; i++) {
+    workArray[i].sort();
+    //this.sortedMatrix.push(workArray[i]);
+  }
+  return workArray;
+}
 
 Matrix.prototype.checkRow = function () {
   let array = this.completeMatrix[0];
@@ -39,13 +52,33 @@ Matrix.prototype.checkRow = function () {
       return false;
     }
   }
-
 }
-// method
-// sample=sort(this.completeMatrix[0][8])
-// method
-// Each row contains all numbers from 1 to 9 with no repeats.
-// for j
-// let sample=sort(this.completeMatrix[0])
+//Matrix.prototype.sortCheckingArray
+
+// Matrix.prototype.checkRow = function () {
+//   let array = this.completeMatrix[0];
+//   for (let i = 0; i < 8; i++) {
+//     array[i].sort();
+//     for (let j = 0; j < 7; j++) {
+      
+//       if (array[i][j] <array[i][j + 1]) {
+//         return outputresult.push(false
+//           break;
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+//   for 1,2,3,3
+//   // outputResult=[true, true,true,..]
+//   // return array[5];
+//   // console.log(array);
+// }  
+// // method
+// // sample=sort(this.completeMatrix[0][8])
+// // method
+// // Each row contains all numbers from 1 to 9 with no repeats.
+// // for j
+// // let sample=sort(this.completeMatrix[0])
 //         0,1,2,3,4,5,6,7,8 
 // sample=[1,2,3,4,5,6,7,8,9],[]
